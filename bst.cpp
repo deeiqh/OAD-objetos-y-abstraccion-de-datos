08850e033f76d2c80ae0d36c5460e037d3fa699e
k-st <kast46.15@gmail.com>
2017-06-26 17:15:29 -0500

    Update bst.cpp

--- bst.cpp
+++ bst.cpp
@@ -53,40 +53,71 @@ bool bst::insert(int value)
 
 struct node2
 {
-    node* nodo;
-    node* sig = NULL;
+    node* nodo = NULL;
+    node2* sig = NULL;
 };
 
 void bst::imprimir_niveles()
 {
-	lista* encadenados1 = NULL;
+	node2* encadenados1 = new node2;
 	if (root !=  NULL){
-		cout << root->value;
+		
+		cout << root->value << '\n';;
+		//cout << "root\n";
 
-        if (root->child[0] != NULL){
-            encadenados1->nodo = root->child[0];
-            cout << root->child[0]->value;
-            if (root->child[1] != NULL){
-                encadenados1 -> sig = root->child[1];
-                cout << root->child[1]-> value;
-            }
-        }else{
-            if (root->child[1] != NULL){
-                encadenados1 = root->child[1];
-                cout << root->child[1]-> value;
-            }
-        }
+		if (root->child[0] != NULL){
+			//cout << "child[0]\n";
+			encadenados1->nodo = root->child[0];
+			cout << root->child[0]->value << ' ';
+			//cout << "child[0]fin\n";
+			
+			if (root->child[1] != NULL){
+				cout << "child[1]\n";
+				encadenados1 -> sig = new node2;
+				encadenados1 -> sig->nodo = root->child[1];
+				cout << "child[1]fin\n";
+				cout << root->child[1]-> value << ' ';				
+			    }
+		}else{
+		    	if (root->child[1] != NULL){
+		        	encadenados1->nodo = root->child[1];
+			        cout << root->child[1]-> value;
+			}
+        	}
+        	cout << '\n';
 	}
-	lista* encadenados2 = NULL;
-	while(encadenados1 != NULL || encadenados2 != NULL){
-		encadenados2 -> nodo = encadenados1-> nodo ->child[0];
-		cout << encadenados1->nodo->child[0]->value;
-		encadenados2 -> sig =  encadenados1-> nodo->child[1];
-		cout << encadenados1->nodo->child[1]->value;
-		encadenados1 = encadenados1 -> sig;
-		if (encadenados1 == NULL)
-			encadenados1 = encadenados2;
-	}
+
+	node2* encadenados2 = new node2;
+	node2* it2 = encadenados2;
+	node2* it1 = encadenados1;
+	while(encadenados1->nodo != NULL){
+		
+		if(it1-> nodo ->child[0] != NULL){
+			it2 -> nodo = it1-> nodo ->child[0];
+			cout << it2->nodo->value << ' ';
+			it2->sig = new node2;
+			it2 = it2->sig;
+		}
+		
+		if(it1-> nodo->child[1] != NULL){
+			it2->nodo =  it1-> nodo->child[1];
+			cout << it2->nodo->value  << ' ';
+			it2->sig = new node2;
+			it2 = it2->sig;
+		}
+		
+		it1 = it1->sig;
+		
+		if(it1->nodo == NULL){
+			cout << '\n';
+			//freeee encadenados1
+			encadenados1 = encadenados2;	
+			it1 = encadenados1;
+			
+			node2* encadenados2 = new node2;
+			node2* it2 = encadenados2;
+		}
+	}	
 }
 int altura()
 {
@@ -100,15 +131,11 @@ int main()
 	bst.insert(1);
 	bst.insert(2);
 	bst.insert(3);
-	bst.insert(4);
-	bst.insert(5);
-	bst.insert(6);
-
+	bst.imprimir_niveles();
 
 
 	return 0;
 }
-
 
 
 
